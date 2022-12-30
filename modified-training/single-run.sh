d8461064fdcb5bc0252f29260d8fd90f58d37ed6
Israel Abebe <se.israel.abebe@gmail.com>
2022-12-30 06:20:57 +0100

    update on run single

--- modified-training/single-run.sh
+++ modified-training/single-run.sh
@@ -1,10 +1,10 @@
 
-model_name="Davlan/afro-xlmr-small"
+model_name=
 language='am'
 
 # Train 
-python run_textclass.py --model_name_or_path $model_name --do_train  --do_eval --train_file SubtaskA/${i}_train.tsv --val_file SubtaskA/${i}_dev_new.tsv --pred_file SubtaskA/${i}_dev.tsv --per_device_train_batch_size 32 --learning_rate 5e-5 --num_train_epochs 3.0 --max_seq_length 128 --data_dir SubtaskA/$model_name --output_dir $j-$model_name-sentiment  --save_steps -1 --overwrite_output_dir
+python run_textclass.py --model_name_or_path "Davlan/afro-xlmr-small" --do_train  --do_eval --train_file SubtaskA/am_train.tsv --val_file SubtaskA/am_dev_new.tsv --pred_file SubtaskA/am_dev.tsv --per_device_train_batch_size 32 --learning_rate 5e-5 --num_train_epochs 3.0 --max_seq_length 128 --data_dir SubtaskA/am --output_dir Davlan/afro-xlmr-small-am-sentiment  --save_steps -1 --overwrite_output_dir
        
 # predict
-python run_predict.py --model_path $j-$i-sentiment --file_name SubtaskA/${i}_dev.tsv --text_column 'tweet' --lang_code $i
+python run_predict.py --model_path Davlan/afro-xlmr-small-am-sentiment --file_name SubtaskA/am_dev.tsv --text_column 'tweet' --lang_code $i
 
