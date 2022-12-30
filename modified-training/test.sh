d872514d891c2ace741c8e5e2b49b75ca06c47e8
Israel Abebe <se.israel.abebe@gmail.com>
2022-12-30 06:36:22 +0100

    update multiple training

--- modified-training/test.sh
+++ modified-training/test.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 
-# cp -r /home/azime/starter_kit/ .
 
-# cd starter_kit
-
-# Davlan/afro-xlmr-small 
-
-
-# model_name="Davlan/afro-xlmr-small"
-
-# 
 for j in  'Davlan/afro-xlmr-small'  'Davlan/afro-xlmr-base' 'Davlan/afro-xlmr-large' 'setu4993/LaBSE' 'castorini/afriberta_large' 'cardiffnlp/twitter-xlm-roberta-base-sentiment' 'jhu-clsp/bernice' 'SI2M-Lab/DarijaBERT'
 do
  
@@ -24,15 +15,4 @@ do
 done
 
 
-# for i in 'am' #'dz' 'ha' 'ig' 'kr' 'ma' 'pcm' 'pt' 'sw' 'ts' 'twi' 'yo'
-# do
-#        python run_textclass.py --model_name_or_path $model_name --do_predict --per_device_train_batch_size 32 --learning_rate 5e-5 --num_train_epochs 1 --max_seq_length 128 --data_dir SubtaskA/$i --output_dir $model_name-$i-sentiment --save_steps -1 --overwrite_output_dir
-# done
-
-
-
-# for i in 'am' 'dz' 'ha' 'ig' 'kr' 'ma' 'pcm' 'pt' 'sw' 'ts' 'twi' 'yo'
-# do
-#    cp -r afro-xlmr-small-$i-sentiment /home/azime/starter_kit/
-# done
 
