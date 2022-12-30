b24e883b772029455c027ef8d4666fcd5f4be91e
Israel Abebe <se.israel.abebe@gmail.com>
2022-12-30 06:15:35 +0100

    train example update

--- modified-training/single-run.sh
+++ modified-training/single-run.sh
@@ -3,7 +3,7 @@ model_name="Davlan/afro-xlmr-small"
 language='am'
 
 # Train 
-python run_textclass.py --model_name_or_path $model_name --do_train  --do_eval  --per_device_train_batch_size 32 --learning_rate 5e-5 --num_train_epochs 3.0 --max_seq_length 128 --data_dir SubtaskA/$model_name --output_dir $j-$model_name-sentiment  --save_steps -1 --overwrite_output_dir
+python run_textclass.py --model_name_or_path $model_name --do_train  --do_eval --train_file SubtaskA/${i}_train.tsv --val_file SubtaskA/${i}_dev_new.tsv --pred_file SubtaskA/${i}_dev.tsv --per_device_train_batch_size 32 --learning_rate 5e-5 --num_train_epochs 3.0 --max_seq_length 128 --data_dir SubtaskA/$model_name --output_dir $j-$model_name-sentiment  --save_steps -1 --overwrite_output_dir
        
 # predict
 python run_predict.py --model_path $j-$i-sentiment --file_name SubtaskA/${i}_dev.tsv --text_column 'tweet' --lang_code $i
